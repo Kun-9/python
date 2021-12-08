ea2653c6e97a628aa78e0221035be49c03184a8d
kun <sus5330@icloud.com>
2021-12-08 15:24:36 +0900

    source tree commit

--- webTest.py
+++ webTest.py
@@ -7,10 +7,6 @@ url = "http://openapi-lib.sen.go.kr"
 data = request.urlopen(url)
 soup = BeautifulSoup(data, 'html.parser')
 
-for loc in soup.select('location') :
-    print("도시 : ", loc.select_one('city').string, end = ", ")
-    print("날씨 : ", loc.select_one('wf').string, end = ", ")
-    print("최저기온 : ", loc.select_one('tmn').string, end = ", ")
-    print("최고기온 : ", loc.select_one('tmx').string)
 
-print()
+
+print("hello")
