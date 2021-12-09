57fc83c8ba1ad0d4db6ed0149a3292ec049e6cd9
kun <sus5330@icloud.com>
2021-12-09 13:56:42 +0900

    commit

--- webTest.py
+++ webTest.py
@@ -1,12 +1,10 @@
 from urllib import request
 from bs4 import BeautifulSoup
+import requests
 
+url = "http://openapi-lib.sen.go.kr/openapi/service/lib/openApi?serviceKey=0Yid8kPxBKOwFYM8goFzmvf6h9Ot3a1H5Iszap8TengJuLyK4A2HOCxWD8XSJ5ulIvFq06/deAnPd/27aYo3IQ==&pageNo=2&numOfRows=5&manageCd=MB&title=1"
 
-url = "http://openapi-lib.sen.go.kr"
-
-data = request.urlopen(url)
+data = request.urlopen(url).read().decode('utf-8', 'replace')
 soup = BeautifulSoup(data, 'html.parser')
 
-
-
-print("hello")
+print(soup)
